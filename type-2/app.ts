39837f5841bbedf5db6b76e1fb5b8e3d0baebb25
mayank0508 <thakurmayank0508@gmail.com>
2021-11-03 11:21:52 +0530

    object

--- type-2/app.ts
+++ type-2/app.ts
@@ -1,9 +1,9 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
-}
+const person: {
+    name: string;
+    age: number;
+} = {
+    name: 'John',
+    age: 34,
+};
 
-const number1 = 5;
-const number2 = 2.8;
-
-const result = add(number1, number2);
-console.log(result);
+console.log(person.age);
