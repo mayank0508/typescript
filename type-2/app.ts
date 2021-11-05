1f09b048e98042df02a4ca7bb662ee5b7715af86
mayank0508 <thakurmayank0508@gmail.com>
2021-11-05 19:43:50 +0530

    arrays

--- type-2/app.ts
+++ type-2/app.ts
@@ -1,11 +1,21 @@
 const person: {
   name: string;
   age: number;
-  hobbies: string[]; // this is how arrays are identified in arrays
-} = {
+  hobbies: string[]; // this is how arrays are identified in typescript
+  favNum: number[]; // this is how arrays of numbers are identified in typescript
+  favSportsPerson: any[];
+ } = {
   name: 'John',
   age: 34,
-  hobbies: ['sports', 'cooking']
+  hobbies: ['sports', 'cooking'],
+  favNum: [7,4,3,2],
+  favSportsPerson: ['Ronaldo', 7]
 };
+
+let favoriteActivity: string[];
+// favoriteActivity = ['sports', 6]; // this will show error as the array is only of the string !!
+
+let favoriteActivity2: any[];
+favoriteActivity2 = ['sports', 6]; // this will not error !
 
 console.log(person.age);
