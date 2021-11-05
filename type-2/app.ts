122c297ab02bfd225306f2459bfdae33e2aa9338
mayank0508 <thakurmayank0508@gmail.com>
2021-11-05 19:36:27 +0530

    arrays

--- type-2/app.ts
+++ type-2/app.ts
@@ -1,9 +1,11 @@
-const person : {
-    name: string;
-    age: number;
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[]; // this is how arrays are identified in arrays
 } = {
-    name: 'John',
-    age: 34,
+  name: 'John',
+  age: 34,
+  hobbies: ['sports', 'cooking']
 };
 
 console.log(person.age);
