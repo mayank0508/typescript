72e491dca537e2e6e7da5c50276ba602a405850d
mayank0508 <thakurmayank0508@gmail.com>
2021-11-12 20:32:07 +0530

    Return type void

--- type-2/app.ts
+++ type-2/app.ts
@@ -1,35 +1,9 @@
-type Combineable = number | string;
-type astypes = 'as-num' | 'as-text';
-
-function combine(
-  n1: Combineable,
-  n2: Combineable,
-  // resultConversion: 'as-num' | 'as-text', // these are called literal types
-  resultConversion: astypes
-) {
-  let result; // this is a important optimization for performance reasons
-  if (
-    (typeof n1 === 'number' && typeof n2 === 'number') ||
-    resultConversion === 'as-num'
-    // typeof resultConversion === 'as-number'
-  ) {
-    result = +n1 + +n2;
-  } else {
-    result = n1.toString() + n2.toString();
-  }
-  //   if (resultConversion === 'as-number') {
-  //     return +n1 + +n2;
-  //   } else {
-  //     return result.toString();
-  //   }
-  return result;
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combinedStringAges = combine('30', '26', 'as-num');
-console.log(combinedStringAges);
+function printResult(num: number) {
+  console.log('Result: ' + num);
+}
 
-const combinedAges = combine(30, 26, 'as-num');
-console.log(combinedAges);
-
-const combinedNames = combine('max', 'me', 'as-text');
-console.log(combinedNames);
+printResult(add(1, 2));
