8d3cb705fe81e0cb7df3b3ef6863f4d9d91c64f1
mayank0508 <thakurmayank0508@gmail.com>
2021-11-13 18:49:25 +0530

    never type

--- type-2/app.ts
+++ type-2/app.ts
@@ -1,6 +1,5 @@
-let userInput: unknown; // this is the unknow type !, here we can store any value with out any error ! 
-let userName: string; 
-
+let userInput: unknown; // this is the unknow type !, here we can store any value with out any error !
+let userName: string;
 
 userInput = 4;
 userInput = 'mayank';
@@ -10,3 +9,9 @@ userName = 'haha';
 // userName = userInput; // here any would not show a issue !
 
 // any is the more flexible of it and unknown, both are kinda same but it all comes down to stricking of the language
+
+function errorthrow(message: string, number: number) {
+  throw { error: message, code: number };
+}
+
+errorthrow('Hi sexy', 432)
