40bc78db16544f807c253e900bbf945412203144
mayank0508 <thakurmayank0508@gmail.com>
2021-11-21 13:27:54 +0530

    nest-gen-js-syntax

--- type-3/src/app.ts
+++ type-3/src/app.ts
@@ -1,5 +1,5 @@
 const button = document.querySelector('button')!;
 
 button.addEventListener('click', () => {
-    console.log('mate got YAYAYAYAYYAYAA 😂😂')
-})
+  console.log('mate got YAYAYAYAYYAYAA 😂😂');
+});
